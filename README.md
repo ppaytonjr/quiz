36c35de077ad66efc763b847d0c48ddcffc8f8a1
Payton <p.paytonjr@outlook.com>
2019-12-04 20:36:02 -0600

    added url to readme file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # quiz
+https://ppaytonjr.github.io/quiz
---
+++ questions.js
@@ -0,0 +1,28 @@
+(function(){
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "Strings in Javascript store what kind of information?",
+        choices: ["text", "pictures", "images" , "booleans"],
+        answer: "text"
+    },
+    {
+        title: "An array is used to store multiple ____ in a single _____",
+        choices: ["objects & numbers", "values & variable", "arguements & functions", "booleans & prompt"],
+        answer: "values & variable"
+    },
+    {
+        title: " Which of the following is a JavaScript HTML method?",
+        choices: ["getElementById", "getArguementByLocation", "getObjectbyElement" , "getElementbyLoc"],
+        answer: "getElementById"}
+]});
+  
